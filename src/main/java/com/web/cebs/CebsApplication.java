001b0f4245c0c32f553e4f4d8a4af6b7f5ef450f
chisb9024 <chishunbingjava@163.com>
2020-08-22 18:42:38 +0800

    Initial commit

---
+++ src/main/java/com/web/cebs/CebsApplication.java
@@ -0,0 +1,13 @@
+package com.web.cebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CebsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/cebs/CebsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.cebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CebsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
